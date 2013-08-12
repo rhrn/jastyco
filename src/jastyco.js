15130e0a7614fc72bb3e4fd6e3846fc12c0dcace
rhrn <maylog@gmail.com>
2013-08-12 10:35:32 +0400

    add build feature

--- src/jastyco.js
+++ src/jastyco.js
@@ -1,32 +1,12 @@
-exports.options = {
-  files: "**/*.jade **/*.coffee **/*.styl"
-}
+var fs = require('fs');
+var gaze = require('gaze');
+var cwd = process.cwd();
 
-exports.jastyco = function (patterns) {
+var jade, coffee, stylus, globule;
 
-  if (patterns === undefined) {
-    console.log('patterns not found. load defaults... "%s"', exports.options.files);
-    patterns = exports.options.files;
-  }
+var jastyco = {
 
-  var fs = require('fs');
-  var gaze = require('gaze');
-
-  var jade, coffee, stylus;
-
-  if (/\.jade/.test(patterns)) {
-    jade = require('jade');
-  }
-
-  if (/\.coffee/.test(patterns)) {
-    coffee = require('coffee-script');
-  }
-
-  if (/\.styl/.test(patterns)) {
-    stylus = require('stylus');
-  }
-
-  var plugins = {
+  plugins: {
     jade: {
       ext: '.html',
       compile: function(src, options) {
@@ -53,45 +33,138 @@ exports.jastyco = function (patterns) {
         return copmiled;
       } 
     }
-  };
+  },
 
-  patterns = patterns.split(" ");
+  file: function(options, filepath) {
+    
+    //console.log ('cwd', cwd, filepath);
+    //console.log ('F:', options, filepath);
 
-  gaze(patterns, function(err, watcher) {
+    if (filepath.charAt(0) == '/' || options.src == options.dest) {
+      filepath = filepath.replace(cwd + '/', '');
+      options.dest = '';
+      options.src = '';
+    } else {
+      options.dest += '/';
+      options.src += '/';
+    }
 
-    var ext, index, filename;
 
-    this.on('all', function(event, filepath) {
-      
-      if (event === 'changed' || event === 'added') {
+    index    = filepath.lastIndexOf('.');
+    //console.log('index', index);
+    filename = filepath.substr(0, index);
+    //console.log('filename', filename);
+    ext      = filepath.substr(index + 1);
+    //console.log('ext', ext);
+    filedest = options.dest + filename + jastyco.plugins[ext].ext;
+    //console.log('filedest', filedest);
 
-        index    = filepath.lastIndexOf('.');
-        filename = filepath.substr(0, index);
-        ext      = filepath.substr(index + 1);
-        filedest = filename + plugins[ext].ext;
+    return {
+      index: index,
+      filepath: filepath,
+      filename: filename,
+      ext: ext,
+      cwd: cwd,
+      filedest: filedest
+    }
 
-        options = {
-          filename: filepath,
-          pretty: true,
-          bare: true
-        };
+  },
 
-        fs.readFile(filepath, function(err, data) {
-          try {
-            compiled = plugins[ext].compile(data.toString(), options);
-            fs.writeFile(filedest, compiled, function(err) {
-              if (!err) {
-                console.log('%s %s to %s', event, filepath, filedest);
-              }
-            });
-          } catch (e) {
-            console.log(e);
+  compile: function(event, file, options) {
+
+    fs.readFile(file.filepath, function(err, data) {
+      //console.log ('complie', event, file, options, data);
+      try {
+        compiled = jastyco.plugins[file.ext].compile(data.toString(), options);
+        fs.writeFile(file.filedest, compiled, function(err) {
+          if (!err) {
+            console.log('%s %s to %s', event, file.filepath, file.filedest);
+            delete data;
           }
         });
-      } else if (event === 'deleted') {
-        console.log('delete', filepath);
+      } catch (e) {
+        console.log(e);
       }
     });
 
-  });
+  }
+};
+
+exports.jastyco = function (options) {
+
+  console.log('options', options);
+
+  if (options.build) {
+    globule = require('globule');
+  }
+
+  if (/\.jade/.test(options.patterns)) {
+    jade = require('jade');
+  }
+
+  if (/\.coffee/.test(options.patterns)) {
+    coffee = require('coffee-script');
+  }
+
+  if (/\.styl/.test(options.patterns)) {
+    stylus = require('stylus');
+  }
+
+  patterns = options.patterns.split(" ");
+
+  var compileOptions = {
+    pretty: true,
+    bare: true
+  };
+
+  var compileOptions;
+
+  if (options.build) {
+    var files;
+    for (var i in patterns) {
+      files = globule.find(patterns[i]);
+      for (var j in files) {
+
+        file = jastyco.file(options, files[j]);
+
+        compileOptions = {
+          pretty: true,
+          bare: true,
+          filename: file.filepath
+        }
+
+
+        //compileOptions.filename = file.filepath;
+
+        jastyco.compile('build', file, compileOptions);
+      }
+    }
+  } else {
+
+    gaze(patterns, function(err, watcher) {
+
+      var file;
+
+      this.on('all', function(event, filepath) {
+        
+        if (event === 'changed' || event === 'added') {
+
+          file = jastyco.file(options, filepath);
+
+          compileOptions = {
+            pretty: true,
+            bare: true,
+            filename: file.filepath
+          }
+
+          jastyco.compile(event, file, compileOptions);
+
+        } else if (event === 'deleted') {
+          console.log('delete, not implemented', file.filepath);
+        }
+      });
+
+    });
+
+  }
 };

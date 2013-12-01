1e80e03407477b8f9c208c672cd7efcd0bf09d21
rhrn <maylog@gmail.com>
2013-12-01 18:58:01 +0400

    add css transition on styles update

--- src/livereload.js
+++ src/livereload.js
@@ -5,6 +5,16 @@
   var scripts = doc.getElementsByTagName('script');
   var links = doc.getElementsByTagName('link');
   var resources = {}, source, parent, next, element;
+  var head = document.getElementsByTagName('head')[0];
+  var html = document.body.parentNode;
+
+  var style = document.createElement("style");
+  var rule = "transition: all .3s ease-out;"
+  var css = [".livejs-loading * { ", rule, " -webkit-", rule, "-moz-", rule, "-o-", rule, "}"].join('');
+
+  style.setAttribute("type", "text/css");
+  head.appendChild(style);
+  style.styleSheet ? style.styleSheet.cssText = css : style.appendChild(document.createTextNode(css));
 
   function getPath(href) {
     parser = doc.createElement('a');
@@ -12,13 +22,13 @@
     return parser.pathname.substr(1);
   }
 
-  html = doc.location.pathname.substr(1);
+  var htmlPath = doc.location.pathname.substr(1);
 
-  if (html.substr(-5) !== '.html') {
-    html += 'index.html';
+  if (htmlPath.substr(-5) !== '.html') {
+    htmlPath += 'index.html';
   }
 
-  resources[html] = true;
+  resources[htmlPath] = true;
 
   var i, link, script, path, ioHost;
   for(i in scripts) {
@@ -55,6 +65,8 @@
 
       if (file.destext === '.css') {
 
+        html.classList.add('livejs-loading');
+
         element = doc.createElement('link');
         element.setAttribute('rel', 'stylesheet');
         element.setAttribute('href', '/' + path + '?now=' + new Date() * 1);
@@ -62,6 +74,10 @@
         next ? parent.insertBefore(element, next) : parent.appendChild(element);
         resources[path] = element;
         parent.removeChild(source);
+  
+        setTimeout(function() {
+          html.classList.remove('livejs-loading');
+        }, 500);
 
       } else if (file.destext === '.js' || file.destext === '.html') {
 
